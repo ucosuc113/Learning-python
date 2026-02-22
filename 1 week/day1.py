2cbc242ee944a45c5468c678048ba3f2c6a13b0d
ucosuc113 <guardaparquebfl3@gmail.com>
2026-02-22 17:18:22 -0500

    ostras

--- 1 week/day1.py
+++ 1 week/day1.py
@@ -1,6 +1,25 @@
-cosas = ["caja", "almacen", "un demonio", "almacen de demonios", "un gato con portales"]
+nota1 = float(input("introduce la primer nota: "))
 
-cosas[3] = "un almacen de demonios con portales"
+if nota1 < 1 or nota1 > 5:
+    print("la nota debe ser entre 1 y 5")
+    exit()
+    
+nota2 = float(input("introduce la segunda nota: "))
 
-for n in cosas:
-    print(n)
+if nota2 < 1 or nota2 > 5:
+    print("la nota debe ser entre 1 y 5")
+    exit()
+    
+nota3 = float(input("introduce la tercera nota: "))
+
+if nota3 < 1 or nota3 > 5:
+    print("la nota debe ser entre 1 y 5")
+    exit()
+    
+
+promedio = (nota1 + nota2 + nota3) / 3
+
+if promedio >= 3.5:
+    print("aprobado con ", promedio)
+else:
+    print("reprobado con ", promedio)
