4d72346f290a51cc412f5225e251cccbc32abbcd
ucosuc113 <guardaparquebfl3@gmail.com>
2026-02-22 17:02:49 -0500

    epa la arepa

--- 1 week/day1.py
+++ 1 week/day1.py
@@ -1,4 +1,6 @@
-def mensaje():
-    print("Estoy aprendiendo python")
+cosas = ["caja", "almacen", "un demonio", "almacen de demonios", "un gato con portales"]
 
-mensaje()
+cosas[3] = "un almacen de demonios con portales"
+
+for n in cosas:
+    print(n)
