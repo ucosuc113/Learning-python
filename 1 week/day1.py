e0f98b2b8d9f3f70c13fac3b2c99a65f6a3f34f2
ucosuc113 <guardaparquebfl3@gmail.com>
2026-02-22 16:39:41 -0500

    interesante

--- 1 week/day1.py
+++ 1 week/day1.py
@@ -1,8 +1,4 @@
 cosafea = 4
 
-if cosafea > 5:
-    print("El número es mayor a 5")
-elif cosafea == 5:
-    print("El número es igual a 5")
-else:
-    print("El número es menor a 5")
+for i in range(128):
+    print(cosafea ** i)
