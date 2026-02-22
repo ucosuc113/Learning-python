b9e6369b9c014f76d5b25e8d73c194f5969a8708
ucosuc113 <guardaparquebfl3@gmail.com>
2026-02-22 18:47:23 -0500

    :0

--- 1 week/Day1-2.py
+++ 1 week/Day1-2.py
@@ -1,1 +1,49 @@
 import json
+
+def inicio():
+    print("desea iniciar sesion? (s/n)")
+
+def sesioniniciar():
+    print("inicie sesion, para salir escriba (n)")
+    with open("sesion.json", "r") as archivo:
+        sesion = json.load(archivo)
+
+        usuario = input("Ingrese su nombre de usuario: ")
+        contraseña = input("Ingrese su contraseña: ")
+    if usuario.lower() == "n" or contraseña.lower() == "n":
+        print("sesion cerrada")
+        return inicio()
+
+    if sesion["username"] == usuario and sesion["password"] == contraseña:
+            print("sesion iniciada correctamente")
+            exit()
+    else:
+            print("usuario o contraseña incorrectos")
+    sesioniniciar()
+
+def registrarse():
+    print("registrese para iniciar sesion")
+    usuario = input("Ingrese su nombre de usuario: ")
+    contraseña = input("Ingrese su contraseña: ")
+
+    if usuario.lower() == "n" or contraseña.lower() == "n":
+        print("volviendo al menu")
+        return inicio()
+
+    sesion = {
+            "username": usuario,
+            "password": contraseña
+        }
+
+    with open("sesion.json", "w") as archivo:
+            json.dump(sesion, archivo, indent=4)
+
+inicio()
+     
+
+if input().lower() == "s":
+
+    sesioniniciar()
+
+elif input().lower() == "n":
+     registrarse()
