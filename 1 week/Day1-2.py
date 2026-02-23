0f06b97b2a5c8e028df52720a6253e7508aefaba
ucosuc113 <guardaparquebfl3@gmail.com>
2026-02-22 19:24:23 -0500

    comentarios sabrosones

--- 1 week/Day1-2.py
+++ 1 week/Day1-2.py
@@ -1,11 +1,15 @@
 import json
 import os
 
+# como funciona?, no se XD, osea si se, pero no lo toquen si no saben lo q hacen XD
+
 def inicio():
     print("desea iniciar sesion? (s/n)")
     return input().lower()
 
 def sesioniniciar():
+
+# por aca, primero envia el mensaje ese, luego abre el json, luego pide el usuario y contraseña, luego recorre el json buscando una coincidencia, si la encuentra, inicia sesion, sino, muestra un mensaje de error y vuelve a pedir los datos ASSAKHDK
     print("inicie sesion, para salir escriba (n)")
     with open("sesion.json", "r") as archivo:
         sesion = json.load(archivo)
@@ -28,6 +32,8 @@ def sesioniniciar():
         return sesioniniciar()
 
 def registrarse():
+
+# aca es similar... pero diferente JAJASKDJH, primero muestra el mensaje, luego pide los datos, luego verifica si el usuario ya existe, si no existe, lo añade al json, sino, muestra un mensaje de error y vuelve a pedir los datos ASSAKHDK
     print("registrese para iniciar sesion")
     usuario = input("Ingrese su nombre de usuario: ")
     contraseña = input("Ingrese su contraseña: ")
@@ -64,6 +70,8 @@ def registrarse():
         return registrarse()
 
 while True:
+
+# hola while :D, este es el menu principal, aqui se pregunta si quieres iniciar sesion o registrarte, dependiendo de la opcion, se llama a la funcion correspondiente, si la opcion es invalida, se vuelve a mostrar el menu ASSAKHDK
     opcion = inicio()
 
     if opcion == "s":
